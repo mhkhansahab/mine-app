ae8ed3433f17dd6aac8cfef60de0e0cb0ae5c6b1
Sajeel Hassan <hsajeel.dev@gmail.com>
2022-07-07 21:33:41 +0500

    Add Countdown timer to mentioned date

--- src/pages/Home/components/SubSection1/index.tsx
+++ src/pages/Home/components/SubSection1/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useContext, useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import {
   CustomHeading,
@@ -31,11 +31,54 @@ const TableContainer = styled("div")(({ theme }) => ({
 }));
 
 const SubSection1: FC = () => {
+  const [timerDays, setTimerDays] = useState<any>(0);
+  const [timerHours, setTimerHours] = useState<any>(0);
+  const [timerMinutes, setTimerMinutes] = useState<any>(0);
+  const [timerSeconds, setTimerSeconds] = useState<any>(0);
+
+  let interval: any;
+
+  const startTimer = () => {
+    const countDownDate = new Date("Aug 23,2022 05:00:00 UTC").getTime();
+
+    interval = setInterval(() => {
+      const now: any = new Date().getTime();
+
+      const distance: any = countDownDate - now;
+
+      const days: any = Math.floor(distance / (24 * 60 * 60 * 1000));
+      const hours: any = Math.floor(
+        (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+      const minutes: any = Math.floor(
+        (distance % (60 * 60 * 1000)) / (1000 * 60)
+      );
+      const seconds: any = Math.floor((distance % (60 * 1000)) / 1000);
+
+      if (distance < 0) {
+        // Stop Timer
+        clearInterval(interval.current);
+        // turnOnMinting();
+      } else {
+        // Update Timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
   return (
     <Container>
       <CustomHeading>Next Giveaway</CustomHeading>
       <CustomContainer>
-        <GradientText>2:21:56:42</GradientText>
+        <GradientText>
+          {timerDays}:{timerHours}:{timerMinutes}:{timerSeconds}
+        </GradientText>
         <CustomSubHeading>Friday, August 23th at 5:00 UTC</CustomSubHeading>
       </CustomContainer>
       <VerticalSpace />
